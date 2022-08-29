c05c9274aa437a68d6514642149d7b393cda5b43
kalluriyogeswararao <kalluriyogesh9@gmail.com>
2022-08-29 03:42:15 +0000

    final

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,1 +1,24 @@
-// Write your code here
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointmentDetails} = props
+  const {title, date, isStared} = appointmentDetails
+
+  const activeStar = isStared
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  return (
+    <li className="appointment">
+      <div className="details">
+        <h1 className="title-head">{title}</h1>
+        <p className="appoin-date">{date}</p>
+      </div>
+      <button type="button" className="star-btn">
+        <img src={activeStar} className="star" alt="star" />
+      </button>
+    </li>
+  )
+}
+
+export default AppointmentItem
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,23 +1,68 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
 import {format} from 'date-fns'
+import AppointmentItem from '../AppointmentItem'
 import './index.css'
 
 class Appointments extends Component {
+  state = {appointmentList: [], title: '', date: '', isStar: false}
+
+  onChangeTitle = event => {
+    this.setState({title: event.target.value})
+  }
+
+  onChangeDate = event => {
+    this.setState({
+      date: event.target.value,
+    })
+  }
+
+  addAppointment = event => {
+    event.preventDefault()
+    const {title, date} = this.state
+    const dateTime = format(new Date(date), 'dd MMMM yyyy, EEEE')
+    const newAppointment = {
+      id: uuidv4(),
+      title,
+      date: dateTime,
+      isStared: false,
+    }
+    this.setState(prevState => ({
+      appointmentList: [...prevState.appointmentList, newAppointment],
+      title: '',
+      date: '',
+    }))
+  }
+
   render() {
+    const {appointmentList, isStar, title, date} = this.state
+    const activeBtn = isStar ? 'stared-btn' : 'normal-btn'
     return (
       <div className="bg-container">
         <div className="card-container">
           <div className="profile-details">
-            <div className="details-container">
+            <form onSubmit={this.addAppointment} className="details-container">
               <h1 className="heading">Appointment</h1>
               <p className="title">TITLE</p>
-              <input type="text" className="input" placeholder="Title" />
+              <input
+                type="text"
+                className="input"
+                placeholder="Title"
+                onChange={this.onChangeTitle}
+                value={title}
+              />
               <p className="title">DATE</p>
-              <input type="date" className="input" />
-              <button type="button" className="button">
+              <input
+                type="date"
+                className="input"
+                onChange={this.onChangeDate}
+                value={date}
+              />
+              <button type="submit" className="button">
                 Add
               </button>
-            </div>
+            </form>
+
             <img
               src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
               alt="appointments"
@@ -26,7 +71,20 @@ class Appointments extends Component {
           </div>
           <hr />
           <div className="all-appointments">
-            <h1 className="side-heading">Appointments</h1>
+            <div className="head">
+              <h1 className="side-heading">Appointments</h1>
+              <button type="button" className={activeBtn}>
+                Stared
+              </button>
+            </div>
+            <ul className="all-appoints">
+              {appointmentList.map(eachAppointment => (
+                <AppointmentItem
+                  appointmentDetails={eachAppointment}
+                  key={eachAppointment.id}
+                />
+              ))}
+            </ul>
           </div>
         </div>
       </div>
